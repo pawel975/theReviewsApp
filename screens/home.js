41a280d926fa4b591498d649dc9ad6b209f9c433
Pawel <pawel.kurek975@gmail.com>
2021-08-15 18:58:56 +0200

    app final version

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,22 @@
 import React, {useState} from 'react';
-import {View, Text, FlatList, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import {globalStyles} from '../styles/global';
 import Card from '../shared/card';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import ReviewForm from './reviewForm';
 
 export default function Home({navigation}) {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const [reviews, setReviews] = useState([
     {
       title: 'Zelda, Breath of Fresh Air',
@@ -24,8 +37,39 @@ export default function Home({navigation}) {
       key: '3',
     },
   ]);
+
+  const addReview = review => {
+    review.key = Math.random().toString();
+    setReviews(currentReviews => {
+      return [review, ...currentReviews];
+    });
+    setModalOpen(false);
+  };
   return (
     <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide">
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
+            <FontAwesome5
+              name="times"
+              solid
+              style={{...styles.modalToggle, ...styles.modalClose}}
+              size={24}
+              color="#333"
+              onPress={() => setModalOpen(false)}
+            />
+            <ReviewForm addReview={addReview}>Hello from the modal</ReviewForm>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
+      <FontAwesome5
+        name="plus"
+        solid
+        style={styles.modalToggle}
+        size={24}
+        color="#333"
+        onPress={() => setModalOpen(true)}
+      />
       <FlatList
         data={reviews}
         renderItem={({item}) => (
@@ -42,3 +86,21 @@ export default function Home({navigation}) {
 }
 
 //When navigating to the second screen, we can pass object in props so the reviewDetails section can display it
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: 'center',
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  },
+});
